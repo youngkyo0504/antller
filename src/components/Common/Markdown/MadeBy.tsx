4e2f1333512db5a0a8f8f3f3775ff6aee2d4395f
keumk <keumky2@gmail.com>
2022-04-19 16:27:55 +0900

    :art: update work detail page

--- src/components/Common/Markdown/MadeBy.tsx
+++ src/components/Common/Markdown/MadeBy.tsx
@@ -1,5 +1,23 @@
 import { Children, FC } from "react";
 import tw from "twin.macro";
+
+const SubRole = ({ role, name }: { role: string; name: string }) => {
+  return (
+    <div tw="mr-5">
+      <p tw="uppercase leading-tight text-xl mt-4   font-bold">{role}</p>
+      <p tw="mt-0 leading-tight font-medium">{name}</p>
+    </div>
+  );
+};
+
+const MainRole = ({ role, name }: { role: string; name: string }) => {
+  return (
+    <div>
+      <p tw="text-2xl uppercase font-bold"> {role}</p>
+      <p tw="text-xl leading-loose font-medium ">{name}</p>
+    </div>
+  );
+};
 interface MadeByProps {
   main?: { key: string; name: string };
   content?: string;
@@ -8,25 +26,17 @@ interface MadeByProps {
 const MadeBy: FC<MadeByProps> = ({ main, children }) => {
   return (
     <div tw="flex flex-col " className="not-prose font-bold">
-      <p tw="text-3xl     font-bold"> PROVISION & PLANNING</p>
-      <p tw="text-2xl leading-loose font-medium ">©ANTTLER</p>
-      <p tw="mt-4 text-3xl   font-bold">VIDEO BY</p>
-      <p tw="text-2xl leading-loose ">나무거꾸로서다 (@treestands.film)</p>
-
+      <MainRole name="©ANTTLER" role="PROVISION & PLANNING" />
+      <MainRole name="나무거꾸로서다 (@treestands.film)" role="video by" />
       <div tw="flex flex-col">
         <div tw="flex">
-          <div tw="mr-5">
-            <p tw="leading-tight text-2xl mt-4  font-bold">DIRECTOR</p>
-            <p tw="mt-0 leading-tight font-medium">서준호</p>
-          </div>
-          <div tw="mr-5">
-            <p tw="leading-tight text-2xl mt-4   font-bold">PRODUCER</p>
-            <p tw="mt-0 leading-tight font-medium">이상윤</p>
-          </div>
-          <div>
-            <p tw="leading-tight text-2xl mt-4  font-bold">MUSIC</p>
-            <p tw="mt-0 leading-tight font-medium  ">우혜원</p>
-          </div>
+          {[
+            { role: "Director", name: "서준호" },
+            { role: "producer", name: "이상윤" },
+            { role: "music", name: "우혜원" },
+          ].map((item) => (
+            <SubRole key={item.name} {...item} />
+          ))}
         </div>
       </div>
     </div>
